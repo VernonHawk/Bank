0f74d471e5ee60c81f0897fc9b9764e837ab7fd9
Igor Morenec <Igor.Morenec@gmail.com>
2018-11-18 20:40:20 +0200

    Added exception handling and logging to the main function.

--- Bank/src/main.cpp
+++ Bank/src/main.cpp
@@ -5,20 +5,27 @@
 
 int main(const int argc, wchar_t* argv[])
 {
-	const auto port = argc == 2 ? argv[1] : U("1337");
+	try
+	{
+		const auto port = argc == 2 ? argv[1] : U("1337");
 
-	const auto uri = web::uri_builder{}
-		.set_scheme(U("http"))
-		.set_host(U("localhost"))
-		.set_port(port)
-		.set_path(U("api"))
-		.to_uri();
+		const auto uri = web::uri_builder{}
+			.set_scheme(U("http"))
+			.set_host(U("localhost"))
+			.set_port(port)
+			.set_path(U("api"))
+			.to_uri();
 
-	util::startServer(uri);
+		util::startServer(uri);
 
-#ifndef NDEBUG
-	std::cout << "Server stopped\n";
-#endif
+	#ifndef NDEBUG
+		std::cout << "Server stopped\n";
+	#endif
+	}
+	catch (const std::exception& ex)
+	{
+		std::wcerr << "Exception occured: " << ex.what() << '\n';
+	}
 
 	return 0;
 }
