663abdb0cc99ad93a809801219c0008530103a4a
Igor Morenec <Igor.Morenec@gmail.com>
2018-12-01 21:00:01 +0200

    Implemented functions for the SHA256 hashing (generic and for passwords). Implemented SaltGenerator to generate unique salts. Removed redundant include in main.

---
+++ Bank/src/Cryptography/Hashing.h
@@ -0,0 +1,21 @@
+﻿#ifndef HASHING_H
+#define HASHING_H
+
+#include <cpprest/asyncrt_utils.h>
+
+namespace util
+{
+	[[nodiscard]]
+	auto hash(const utility::string_t&) -> utility::string_t;
+
+	[[nodiscard]]
+	auto hashPassword(const utility::string_t& pass) -> std::pair<utility::string_t, utility::string_t>;
+
+	[[nodiscard]] inline
+	auto hashPassword(const utility::string_t& pass, const utility::string_t& salt)
+	{
+		return hash(pass + salt);
+	}
+}
+
+#endif
