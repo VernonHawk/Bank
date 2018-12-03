7e7534e335a52fdab24a692f88088f00f419a727
Andrew Alexeev <andrey.alexeev.1998@gmail.com>
2018-12-03 21:21:32 +0200

    удалил лишнее #2

--- Bank/src/main.cpp
+++ Bank/src/main.cpp
@@ -6,7 +6,6 @@
 
 int main(const int argc, wchar_t* argv[])
 {
-	std::cout<<(*DBHandler::getInstance()->getCard("1111111111111111")).balance();
 	try
 	{
 		const auto port = argc == 2 ? argv[1] : U("1337");
